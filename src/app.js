71acfca6ddcca25668c5414d9048923108e54e1a
NovaGlyphos <ag12015002@gmail.com>
2024-10-12 00:07:07 +0530

    playing with the routes

--- src/app.js
+++ src/app.js
@@ -1,27 +1,12 @@
 const express = require("express");
 const app = express();
 
-app.use("/user",(req,res) => {
-    res.send("HAHAHAAHA");
-})
 
-
-app.get("/user",(resq,res) => {
+app.get("/user/:userId/:name/:password",(req,res) => {
+    console.log(req.params);
     res.send({firstName:"Ayush",lastName:"Guleria"});
 })
 
-app.post("/user",(req,res) => {
-    //saving data to db
-    res.send("Data successfully saved to the database");
-})
-
-app.delete("/user",(req,res) => {
-    res.send("data deleted successfully")
-})
-
-app.use("/test", (req,res) => {
-    res.send("hello from test");
-})
 
 app.listen(3000,() => {
     console.log("server is running on port 3000");
