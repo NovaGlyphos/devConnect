2e0319452fd289171fbe10d4724117f5afe963dc
NovaGlyphos <ag12015002@gmail.com>
2024-10-12 02:56:31 +0530

    playing with multiple route handlers

--- src/app.js
+++ src/app.js
@@ -1,13 +1,26 @@
 const express = require("express");
 const app = express();
 
+app.use("/user", [(req,res,next) => {
+    //route handler
+    // res.send("route Handler 1");
+    console.log("handeling the route user");
+    next();
 
-app.get("/user/:userId/:name/:password",(req,res) => {
-    console.log(req.params);
-    res.send({firstName:"Ayush",lastName:"Guleria"});
-})
+    // res.send("respone 1!!");
+}, 
+    (req,res,next) => {
+    console.log("called the 2nd route handler");
+    next();
+    // res.send("resonse 2!!");
+    
+},
+(req,res) => {
+    console.log("called the 3rd route handler");
+    res.send("resonse 3!!");
+}]);
 
 
-app.listen(3000,() => {
+app.listen(3000,() => { 
     console.log("server is running on port 3000");
 });
