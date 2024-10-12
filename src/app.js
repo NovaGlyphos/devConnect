9171acee0fa8ef61c0e112c62b77f5d73ef87bd4
NovaGlyphos <ag12015002@gmail.com>
2024-10-12 17:05:43 +0530

    working with error handling

--- src/app.js
+++ src/app.js
@@ -1,33 +1,24 @@
 const express = require("express");
 const app = express();
 
-const {adminAuth,userAuth} = require("./middlewares/auth");
-//handle auth middleware for GET,POSt,DELETE
-//logic of checking if the request is sctaully made by the admin (request is authorized)
 
-//this is a middleware that will only br called for /admin
-app.use("/admin" , adminAuth);
-
-app.post("/user/login",(req,res)=>{
-    res.send("user logged in successfully")
+app.get("/getUserData",(req,res) => {
+    try{
+        //logic of db call and get user data
+        throw new Error("dbjfbdj")
+        res.send("user data sent");
+    } catch(err){
+        res.status(500).send("some error occcured contact user team");
+    }
+     
 })
 
-app.get("/user",userAuth,(req,res) => {
-    res.send("user data sent"); 
-})
-
-app.get("/admin/getAllData", (req,res) => {
-    //logic of fetching all data
-    
-    res.send("All data sent                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                             ")
-
-});
-
-app.get("/admin/deleteUser", (req,res) => {
-    //logic of deleting
-    res.send("Deleted user");
-
-});
+/* app.use("/",(err,req,res,next) => {
+    if(err){
+        //log the error
+        res.status(500).send("something went wrong");
+    }
+}) */
 
 app.listen(3000,() => { 
     console.log("server is running on port 3000");
