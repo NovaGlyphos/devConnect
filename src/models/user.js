d618802391f2ad5b5b92b7e71ed162cf5bc70782
NovaGlyphos <ag12015002@gmail.com>
2024-10-13 17:56:23 +0530

    data-sanitisation

--- src/models/user.js
+++ src/models/user.js
@@ -2,25 +2,46 @@ const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
     firstName: {
-        type: String
+        type: String,
+        required: true,
     },
     lastName: {
         type: String
     },
     emailId: {
-        type: String
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
     },
     password: {
-        type: String
+        type: String,
+        required: true,
     },
     age: {
-        type:Number
+        type: Number
     },
     gender: {
-        type:String
+        type: String
+    },
+    photoUrl:{
+        type:String,
+        default:"https://th.bing.com/th/id/OIP.UCL14P68pN74fd8s7TLgZwAAAA?rs=1&pid=ImgDetMain",
+    },
+    about: {
+        type:String,
+        default:"This is default about of the user",
+    },
+    skills: {
+        type:[String],
     }
 });
 
-const User = mongoose.model("User",userSchema);
+const User = mongoose.model("User", userSchema);
+
+// Ensure unique index is created
+User.init().then(() => {
+    console.log("Indexes ensured");
+}).catch(err => console.log("Index error: ", err));
 
 module.exports = User;
